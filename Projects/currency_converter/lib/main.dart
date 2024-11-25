74d753ac86c99c03389448d2094aa600b0698de0
joaovtfor <joaovtfor@hotmail.com>
2024-11-25 10:52:36 -0300

    `Fix` - main.dart adjustments

--- Projects/currency_converter/lib/main.dart
+++ Projects/currency_converter/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'app/controllers/darkModeController.dart';
+import 'app/controllers/dark_mode_controller.dart';
 import 'app/views/home-view.dart';
 
 main() {
@@ -13,7 +13,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const HomeView(),
+      home: HomeView(),
       theme: ThemeData(
           primarySwatch: Colors.red,
           brightness: DarkModeController.instance.isDarkTheme
