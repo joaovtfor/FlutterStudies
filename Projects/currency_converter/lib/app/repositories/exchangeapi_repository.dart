171390f0704b998335ea2194ff8694d068c90050
joaovtfor <joaovtfor@hotmail.com>
2024-12-12 11:24:33 -0300

    `Add` - Mocking API (WIP)

--- Projects/currency_converter/lib/app/repositories/exchangeapi_repository.dart
+++ Projects/currency_converter/lib/app/repositories/exchangeapi_repository.dart
@@ -2,17 +2,19 @@ import 'package:currency_converter/app/models/exchangeapi_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class ExchangeapiRepository {
+class ExchangeApiRepository {
+  ExchangeApiModel exchangeApi;
+
+  ExchangeApiRepository(this.exchangeApi);
+
   Future<ExchangeApiModel> fetchExchangeAPI({baseCurrency}) async {
-    var url = Uri.parse('https://open.er-api.com/v6/latest/$baseCurrency');
-    final response = await http
+    final url = Uri.parse('https://open.er-api.com/v6/latest/$baseCurrency');
+    var response = await http
         .get(url, headers: {'Content-Type': 'application/json; charset=UTF-8'});
 
-    final data = jsonDecode(response.body);
-
     if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
       final dataValues = ExchangeApiModel.fromJson(data);
-
       return dataValues;
     } else {
       throw Exception('Failed to load data');
--- Projects/currency_converter/test/app/repositories/exchangeapi_repository_test.dart
+++ Projects/currency_converter/test/app/repositories/exchangeapi_repository_test.dart
@@ -1,10 +1,26 @@
-import 'package:currency_converter/app/repositories/exchangeapi_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'dart:convert';
 
-main() {
-  final repository = ExchangeapiRepository();
+import 'package:currency_converter/app/models/exchangeapi_model.dart';
+import 'package:currency_converter/app/repositories/exchangeapi_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class ExchangeApiMock extends Mock implements ExchangeApiRepository {}
+
+void main() {
+  final exchangeApi = ExchangeApiMock();
+
+  final repository = ExchangeApiRepository(exchangeApi as ExchangeApiModel);
+
   test('Must show an API repo currency type', () async {
+    when(() => exchangeApi.fetchExchangeAPI(baseCurrency: 'USD'))
+        .thenAnswer((_) async => ExchangeApiModel.fromJson(jsonDecode(json)));
+
     final obj = await repository.fetchExchangeAPI(baseCurrency: 'USD');
+    print(obj.rates?.bRL);
     expect(obj.baseCode, 'USD');
   });
 }
+
+const json =
+    '{"result":"success","time_last_update_utc":"Thu, 12 Dec 2024 00:02:32 +0000","time_next_update_utc":"Fri, 13 Dec 2024 00:22:22 +0000","base_code":"BRL","rates":{"USD":1,"BRL":6.3232,"BTN":84.885405,"EUR":0.952205,"GBP":0.784118,}}';
